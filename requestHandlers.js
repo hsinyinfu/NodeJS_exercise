0dff855946498db570b36ce82e97903bd02a582e
hsinyinfu <peter0930260535@gmail.com>
2017-08-11 10:53:29 +0800

    使用querystring選取要顯示的是text的內容

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,4 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 exports.start = function(response){
 	console.log("Request handler 'start' was called.");
@@ -25,6 +25,6 @@ exports.start = function(response){
 exports.upload = function(response, postData){
 	console.log("Request handler 'upload' was called.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent "+ postData);
+	response.write("You've sent "+ querystring.parse(postData).text);
 	response.end();
 }
